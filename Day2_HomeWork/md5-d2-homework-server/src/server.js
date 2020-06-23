bbee4ab209ee76874ec136c06a20e3c15d6eee01
klevinb <klevin.bazaiti98@gmail.com>
2020-06-23 22:11:53 +0200

    cors module

--- Day2_HomeWork/md5-d2-homework-server/src/server.js
+++ Day2_HomeWork/md5-d2-homework-server/src/server.js
@@ -1,7 +1,10 @@
 const express = require("express")
 const studentRoute = require("./services/students")
+const cors = require("cors")
 
 const server = express()
+
+server.use(cors())
 
 server.use(express.json())
 
