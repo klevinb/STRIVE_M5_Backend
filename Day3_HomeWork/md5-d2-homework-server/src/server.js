68ce68d711727102e4273a6724bec33eab3c5b8e
klevinb <klevin.bazaiti98@gmail.com>
2020-06-24 22:48:49 +0200

    server use errorhandlers

--- Day3_HomeWork/md5-d2-homework-server/src/server.js
+++ Day3_HomeWork/md5-d2-homework-server/src/server.js
@@ -2,13 +2,18 @@ const express = require("express")
 const listEndpoints = require("express-list-endpoints")
 const studentRoute = require("./services/students")
 const portfoliosRoute = require("./services/portfolio")
+const {
+    notFound,
+    notAuthorized,
+    generalError
+} = require("./handleErrors")
+
 const cors = require("cors")
 
 
 const server = express()
 
 server.use(cors())
-
 server.use(express.json())
 
 const port = process.env.PORT || 3003
@@ -18,6 +23,9 @@ server.use("/projects", portfoliosRoute)
 
 console.log(listEndpoints(server))
 
+server.use(notFound)
+server.use(notAuthorized)
+server.use(generalError)
 
 server.listen(port, () => {
     console.log(`Server running on port ${port}`)
