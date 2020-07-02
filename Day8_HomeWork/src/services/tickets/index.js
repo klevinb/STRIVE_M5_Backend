0c2869ae6dce692cdfa8b9d149624763af72c0db
klevinb <klevin.bazaiti98@gmail.com>
2020-07-02 07:32:49 +0200

    route for creating/saveing/sending pdf to the users

--- Day8_HomeWork/src/services/tickets/index.js
+++ Day8_HomeWork/src/services/tickets/index.js
@@ -7,6 +7,8 @@ const { check, validationResult } = require("express-validator")
 
 
 const sgMail = require('@sendgrid/mail')
+const PdfPrinter = require('pdfmake')
+const qr = require("qrcode")
 
 const { Transform } = require('json2csv')
 const pump = require("pump")
@@ -42,12 +44,66 @@ router.post("/", [
         next(err)
     } else {
         const newTicket = { id: uniqid(), ...req.body }
-        const tickets = await readTickets(invitationsPath)
-        tickets.push(newTicket)
-        await writeTickets(invitationsPath, tickets)
+        var fonts = {
+            Roboto: {
+                normal: 'node_modules/roboto-font/fonts/Roboto/roboto-regular-webfont.ttf',
+                bold: 'node_modules/roboto-font/fonts/Roboto/roboto-bold-webfont.ttf',
+                italics: 'node_modules/roboto-font/fonts/Roboto/roboto-italic-webfont.ttf',
+                bolditalics: 'node_modules/roboto-font/fonts/Roboto/roboto-bolditalic-webfont.ttf'
+            }
+        };
+        var printer = new PdfPrinter(fonts);
+        var docDefinition = {
+            pageMargins: [150, 50, 150, 50],
+            content: [
+                { text: 'YEEYY PARTY TIME!!', fontSize: 25, background: 'yellow', italics: true },
+                "                                                                      ",
+                `Thanks for joining ${newTicket.firstname} ${newTicket.secondname}!`,
+                'I hope you will have fun, and dont forget to get drunk!',
+            ]
+        }
+        var pdfDoc = printer.createPdfKitDocument(docDefinition);
+        pdfDoc.pipe(fs.createWriteStream(join(pdfSaver, `${newTicket.id}.pdf`)));
+        pdfDoc.end();
+
+
+        const pdfData = join(__dirname, `pdf/${newTicket.id}.pdf`)
+
+        sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+
+        const sendEmail = async () => {
+            fs.readFile(pdfData, function (err, data) {
+                let data_base64 = data.toString('base64')
+                sgMail.send({
+                    to: `${newTicket.email}`,
+                    from: 'developersParty@gmail.com',
+                    subject: 'Party Ticket',
+                    text: 'report',
+                    attachments: [{
+                        filename: `${newTicket.firstname}Ticket.pdf`,
+                        content: data_base64,
+                        type: 'application/pdf',
+                        disposition: 'attachment',
+                    }],
+                })
+            })
+        }
+
+        const intervalFunc = async () => {
+            const tickets = await readTickets(invitationsPath)
+            tickets.push(newTicket)
+            await writeTickets(invitationsPath, tickets)
+            console.log("here")
+        }
+
+        setTimeout(sendEmail, 1000);
+        setTimeout(intervalFunc, 5000);
+
         res.status(201).send("Created")
     }
 })
+
+
 
 
 
